b44f2834e9d2c30713a67bae5aee779666215180
Viacheslav Kisel <primazp@icloud.com>
2015-11-10 23:06:56 +0300

    Add Strategy example

---
+++ design-patterns/behavioral/Strategy.ts
@@ -0,0 +1,56 @@
+// Strategy
+interface Formatter {
+    process(title: string, text: string[]) : string;
+}
+
+// Concrete Strategy A
+class HTMLFormatter implements Formatter {
+    process(title: string, lines: string[]) {
+        var text = '';
+        for(var i = 0; i < lines.length; i++) {
+            text += '    <p>' + lines[i] + '</p>\n'
+        }
+        return '<html>\n' +
+            '  <head>\n' +
+            '    <title>' + title + '</title>\n' +
+            '  </head>\n' +
+            '  <body>\n' +
+            text +
+            '  </body>\n' +
+            '</html>\n';
+    }
+}
+
+// Concrete Strategy B
+class PlainTextFormatter implements Formatter {
+    process(title: string, lines: string[]) {
+        return title + '\n\n' + lines.join('\n');
+    }
+}
+
+// Context
+class Report {
+    constructor(private title: string,
+                private lines: string[],
+                private formatter: Formatter) {}
+
+    setFormatter(formatter: Formatter) {
+        this.formatter = formatter;
+    }
+
+    print() : void {
+        console.log(this.formatter.process(this.title, this.lines));
+    }
+}
+
+var report = new Report(
+    'Sample Report',
+    ['Lorem ipsum.', 'Dolor sit amet.'],
+    new HTMLFormatter()
+);
+report.print();
+
+console.log('-----\n');
+
+report.setFormatter( new PlainTextFormatter() );
+report.print();
